51fa303e70f4b43742019edee28da1e83005e888
svolodarskyi <sergii.volodarskyi@gmail.com>
2023-12-04 09:12:07 -0700

    day 1, solution 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent_of_code_2023
+
+This repository contains solution for the Advent Of Code 2023
---
+++ day1/1.py
@@ -0,0 +1,18 @@
+with open("./input.txt", "r") as f:
+    task_input = [i.strip() for i in f]
+
+
+def first_digit(data: str, direction: str) -> int:
+    if direction == 'f':
+        data = data
+    if direction == 'b':
+        data = data[::-1]
+
+    for _ in data:
+        if _.isnumeric():
+            return str(_)
+
+sub_totals = [int(first_digit(i, 'f') + first_digit(i, 'b')) for i in task_input]
+total = sum(sub_totals)
+
+print(total)
