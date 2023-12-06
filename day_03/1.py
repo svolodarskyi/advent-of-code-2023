d1adcdd06369b78e969ec629c978d337da504568
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2023-12-06 17:44:58 +0000

    day 3, part 1 update

--- day_03/1.py
+++ day_03/1.py
@@ -11,25 +11,21 @@ numb = ''
 numb_start_loc = None
 numb_end_loc = None
 
-for line_id in line_idx[1:2]:
+#collect locations of the special characters
+#collect starting and ending location of the numbers
+for line_id in line_idx:
     for char_id in char_idx:
         char_curr = input_file[line_id][char_id]
         if not char_curr.isdigit() and char_prev.isdigit():
             numb_end_loc = char_id - 1
-            print("numb_end_loc", numb_end_loc)
             numb_loc.setdefault(line_id, []).append(f"{numb}-{numb_start_loc}-{numb_end_loc}")
             numb = ""
         elif not char_curr.isdigit() and char_curr != '.':
-            print(char_curr)
             char_loc.setdefault(line_id, []).append(char_id)
         elif char_curr.isdigit() and numb == '':
             numb+=char_curr
             numb_start_loc = char_id
-            print("numb_start_loc", numb_start_loc)
         elif char_curr.isdigit():
             numb+=char_curr
-            print("char_curr", char_curr, "whole", numb)
         char_prev = char_curr
-            
-print(char_loc)
-print(numb_loc)
+
