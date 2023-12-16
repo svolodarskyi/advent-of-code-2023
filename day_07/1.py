051faa111820f3025d406b1f7b83fff1b7653f70
svolodarskyi <sergii.volodarskyi@gmail.com>
2023-12-16 07:19:14 -0700

    day 7 part 1 wip

--- day_07/1.py
+++ day_07/1.py
@@ -24,14 +24,35 @@ def encode_ch(ch: str) -> str:
 def encode_hand(hand: str)-> str:
     return "".join([encode_ch(ch) for ch in hand])
 
-five_kind = []
-four_kind = []
-full_house = []
-three_kind = []
-two_pair = []
-one_pair = []
-high_card = []
 
-for items in hands[:4]:
-    hand, weight = items.split()
-    print(encode_hand(hand))
+def define_type(sorted_count: list[int]) -> int:
+    ''' 
+        defines the type(ranking) being five_of_a_kind as 1 and high_card as 8.
+        ranking in ascending order, being 1 is "highter" than 8.
+        five_of_a_kind > four_of_a_kind > full_house > three_kind > two_pair > one_pair > high_card
+    '''
+    if sorted_count == [5]:
+        return 1
+    elif sorted_count == [1,4]:
+        return 2
+    elif sorted_count == [2,3]:
+        return 3
+    elif sorted_count == [1,1,3]:
+        return 4
+    elif sorted_count == [1,2,2]:
+        return 5
+    elif sorted_count == [1,1,1,2]:
+        return 6
+    elif sorted_count == [1,1,1,1,1]:
+        return 7
+    
+
+hand_types = {}
+encoded_hands = {encode_hand(hand.split()[0]):hand.split()[1] for hand in hands}
+
+for encoded_hand in encoded_hands.keys():
+    hand_counted = dict(Counter(encoded_hand))
+    hand_counted_sorted = sorted(hand_counted.values())
+    hand_types.setdefault(define_type(hand_counted_sorted), []).append(encoded_hand)
+
+print(sorted(hand_types[2]))
